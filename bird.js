867815c80c40c735e18534158af11de87af34892
Izabel Rosen <izabelrosen@gmail.com>
2019-11-22 16:45:24 +0100

    Add pipes

--- bird.js
+++ bird.js
@@ -2,7 +2,8 @@ function Bird() {
   this.y = height / 2;
   this.x = 64;
 
-  this.gravity = 1;
+  this.gravity = 0.6;
+  this.lift = -25;
   this.velocity = 0;
 
   this.show = function() {
@@ -11,11 +12,13 @@ function Bird() {
   }
 
   this.up = function() {
-    this.velocity -= this.gravity * 10;
+    this.velocity += this.lift; 
+    console.log(this.velocity);
   }
 
   this.update = function() {
     this.velocity += this.gravity;
+    this.velocity += 0.9;
     this.y += this.velocity;
 
     if (this.y > height) {
--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 let bird;
+let pipes = [];
 
 function setup() {
   createCanvas(400, 600);
   bird = new Bird();
+  pipes.push(new Pipe());
 }
 
 function draw() {
   background(0, 4, 200);
   bird.show();
   bird.update();
+
+  if (frameCount % 100 == 0) {
+    pipes.push(new Pipe());
+  }
+
+  for (let i = 0; i < pipes.length; i++) {
+    pipes[i].show();
+    pipes[i].update();
+  }
 }
 
 function keyPressed() {
-  if (key == " ") {
+  if (key == ' ') {
     bird.up();
   }
 }
