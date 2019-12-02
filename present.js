82d69e03ce18911c10f3fd5bebd2d59d45fd1591
Amanda Stensland <amanda.stensland@bonniernews.se>
2019-12-02 09:48:11 +0100

    Update present.js - Add master-branch template

--- present.js
+++ present.js
@@ -1,25 +1,40 @@
-function christmasPresent() {
-  this.y = random(height);
-  this.speed = 2;
-  this.x = width;
-  this.height = 32;
-  this.width = 32;
+/****************************************************
+ * present.js
+ *
+ ****************************************************/
 
-  this.show = function() {
-    image(presentImg, this.x, this.y, this.width, this.height);
+class Present {
+  y = random(height);
+  speed = 2;
+  x = width;
+  height = 32;
+  width = 32;
+
+
+  /****************************************************
+   * Show the present image by using image() from p5, 
+   * together with x, y, width and height
+   * @custom
+   ****************************************************/
+  show() {
+  
   }
 
-  this.update = function() {
-    this.x -= this.speed;
+
+  /****************************************************
+   * Make the present start outside of the canvas to the right and
+   * move to the left. 
+   * Use x and speed.
+   ****************************************************/
+  update() {
+    
   }
 
-  this.hits = function(wasp) {
-    if (wasp.y > this.y && wasp.y < this.y + this.height) {
-      if (wasp.x > this.x && wasp.x < this.x + this.width) {
-        console.log("HITS");
-        return true;
-      }
-    }
-    return false;
+
+  /****************************************************
+   * Here you want the wasp to hit the present
+   ****************************************************/
+  hits(wasp) {
+
   }
 }
